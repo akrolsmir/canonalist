a1b9989b114d0ee586a183a2b33eb4f324ad059c
Austin Chen <akrolsmir@gmail.com>
2020-01-06 19:05:50 -0800

    Update project name from component

--- project-vue.js
+++ project-vue.js
@@ -1,9 +1,13 @@
 Vue.component('project-component', {
   props: {
-    project: Object,
+    value: Object, // Should be a project object
     currentPageId: String,
   },
   methods: {
+    // Adapted from https://simonkollross.de/posts/vuejs-using-v-model-with-objects-for-custom-components
+    update(key, value) {
+      this.$emit('input', { ...this.value, [key]: value })
+    },
     clickPage(pageId) {
       this.$emit('change-page', pageId);
     }
@@ -11,9 +15,9 @@ Vue.component('project-component', {
   template: `
   <div class="panel">
     <h4>PROJECT</h4>
-    <input type="text" v-model="project.name">
+    <input type="text" v-model="value.name" v-shortkey.avoid>
     <ul>
-      <li v-for="(page, i) in project.pages">
+      <li v-for="(page, i) in value.pages">
         <a href="#" @click="clickPage(page.id)">
           {{ "Page " + (i + 1) + (page.id == currentPageId ? '*' : '') }}
         </a>
